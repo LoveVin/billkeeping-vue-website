ceaf106c78cf670ffb23b005b566927440ba3ba6
twinklelixn <xiaonanli@163.com>
2020-06-11 21:57:38 +0800

    deploy

---
+++ precache-manifest.f918ea93e946f1f3f57f34c282b47364.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9a3e98c2d45c5664f2",
+    "url": "/billkeeping-vue-website/css/app.709a6a68.css"
+  },
+  {
+    "revision": "22916d16d3db9cb8039c1cf4e43b383b",
+    "url": "/billkeeping-vue-website/index.html"
+  },
+  {
+    "revision": "3c9a3e98c2d45c5664f2",
+    "url": "/billkeeping-vue-website/js/app.8367dc5e.js"
+  },
+  {
+    "revision": "9f0878dacaa86d360a9b",
+    "url": "/billkeeping-vue-website/js/chunk-vendors.afb3bd62.js"
+  },
+  {
+    "revision": "e52df7d1c6223d4ad633fe7d599841d7",
+    "url": "/billkeeping-vue-website/manifest.json"
+  },
+  {
+    "revision": "dbade1da52bbb65de5c1faa0c0c54fc0",
+    "url": "/billkeeping-vue-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/billkeeping-vue-website/robots.txt"
+  }
+]);
